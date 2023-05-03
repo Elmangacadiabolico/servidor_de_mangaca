58c55af428d479278dad3e22b70fbfbcc9557f33
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-05-02 21:05:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://wicher:<password>@hector-1.d6ivwpy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Emanuel:<password>@cluster0.ios0wdb.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -13,7 +13,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server    (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
